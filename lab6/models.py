5a7829d8598b4e9b620293d002fb3f151d9ebbac
anyakulinchenko <55593314+anyakulinchenko@users.noreply.github.com>
2020-12-07 14:21:22 +0200

    Update models.py

--- lab6/models.py
+++ lab6/models.py
@@ -11,15 +11,6 @@ class Student(db.Model):
     total_rating = db.Column('total_rating', db.Integer)
 
 
-class Rating(db.Model):
-    __tablename__ = 'ratings'
-    rid = db.Column('rid', db.Integer, primary_key=True)
-    subject = db.Column('subject', db.VARCHAR(length=30))
-    svalue = db.Column('svalue', db.Integer)
-    sid = db.Column(db.Integer, db.ForeignKey(Student.sid))
-    tmp = db.relationship('Student')
-
-
 class Teacher(db.Model):
     __tablename__ = 'teachers'
     tid = db.Column('tid', db.Integer, primary_key=True)
